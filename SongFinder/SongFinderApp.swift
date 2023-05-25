080fdb126298ac68eadf115d28fdbe38ebbaba8c
cyrus chen <pschen@lcs.on.ca>
2023-05-25 15:31:17 -0400

    Initial Commit

---
+++ SongFinder/SongFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongFinderApp.swift
+//  SongFinder
+//
+//  Created by Cyrus Chen on 25/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct SongFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
