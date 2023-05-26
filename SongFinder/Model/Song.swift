cbf4366624af7984ab2c1eaf7b5156a1a598d041
cyrus chen <pschen@lcs.on.ca>
2023-05-26 09:13:13 -0400

    Created a model to match the response from the endpoint.

---
+++ SongFinder/Model/Song.swift
@@ -0,0 +1,20 @@
+//
+//  Song.swift
+//  SongFinder
+//
+//  Created by Cyrus Chen on 26/5/2023.
+//
+
+import Foundation
+
+struct Song: Codable {
+    
+    let trackId: Int
+    let artistName: String
+    let trackName: String
+    let artistViewUrl: String
+    let collectionViewUrl: String
+    let previewUrl: String
+    let artworkUrl100: String
+}
+
